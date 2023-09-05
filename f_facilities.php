f9504214e65b91ed53404b0af6ee348d4aed7ab7
S N W Fernando <73053183+NipuniFernando@users.noreply.github.com>
2023-09-05 20:36:39 +0530

    Add files via upload

---
+++ f_facilities.php
@@ -0,0 +1,103 @@
+<?php include 'f_header.php'; ?>
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs" data-aos="fade-in">
+      <div class="container">
+        <h2>OUR FACILITIES TO OUR STUDENTS</h2>
+        <p>CKC College Offer Great facilities to our Student for better Learning Experience</p>
+      </div>
+    </div><!-- End Breadcrumbs -->
+
+    <!-- ======= Events Section ======= -->
+    <section id="events" class="events">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-md-6 d-flex align-items-stretch">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/facilities/vertual_learning.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><a href="">Vertual Learning Environment</a></h5>
+                <p class="card-text">We Offer Fully Feateured VLE Syetem to our students for self study.</p>
+                <a href="f_facilities_view.php" class="get-started-btn">View Demo</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 d-flex align-items-stretch">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/facilities/exam.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><a href="">Online Exam System</a></h5>
+                <p class="card-text">We Provide online exam facility to our students</p>
+                <a href="f_facilities_view.php" class="get-started-btn">View Demo</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 d-flex align-items-stretch">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/facilities/distance_learning.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><a href="">Distance Learning With Lecturers</a></h5>   
+                <p class="card-text">We Provide Distance Learning With Lecturers using online live classrooms</p>
+                <a href="f_facilities_view.php" class="get-started-btn" >View Demo</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 d-flex align-items-stretch">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/facilities/online_learning.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><a href="">Fully Online Cources with E-Learning</a></h5>   
+                <p class="card-text">We Provide Fully online Learning With online video tutorials and exams.</p>
+                <a href="f_facilities_view.php" class="get-started-btn" >View Demo</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 d-flex align-items-stretch">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/facilities/classroom_learning.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><a href="">Classroom Learning</a></h5>   
+                <p class="card-text">We Provide Inhouse learning in CKC Classrooms</p>
+                <a href="f_facilities_view.php" class="get-started-btn" >View Demo</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 d-flex align-items-stretch">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/facilities/computer_lab.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title"><a href="">Fully Feateured Computer Labs</a></h5>   
+                <p class="card-text">We Provide Inhouse learning Computer practical sessions in CKC Full Feateured Labs</p>
+                <a href="f_facilities_view.php" class="get-started-btn" >View Demo</a>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+    </section><!-- End Events Section -->
+
+  </main><!-- End #main -->
+
+ <?php include 'f_footer.php'; ?>
---
+++ f_facilities_view.php
@@ -0,0 +1,55 @@
+<?php include 'f_header.php'; ?>
+
+<!-- Video -->
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<!------ Include the above in your HEAD tag ---------->
+<style>
+  .vid {
+      position: relative;
+      padding-bottom: 56.25%;
+      padding-top: 30px; height: 0; overflow: hidden;
+  }
+   
+  .vid iframe,
+  .vid object,
+  .vid embed {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+  }
+</style>
+
+<!-- Reference: http://avexdesigns.com/responsive-youtube-embed/ -->
+<br><br><br><br>
+
+</div><!--./container -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <h2 style="color:#0061f2;">Vertual Learning Management</h2><br>
+            <h4>We provide best VLE System experience to our students.</h4>      
+            <img src="assets/img/facilities/vertual_learning.jpg" class="img-responsive center-block" alt="vle">
+            <br><br><br><br>
+        </div><!--.col -->
+        
+        <div class="col-md-8">
+            <div class="vid">
+                <iframe width="560" height="315" src="//www.youtube.com/embed/ac7KhViaVqc" allowfullscreen=""></iframe>
+
+            </div><!--./vid --> 
+        </div><!--.col -->
+
+        
+    </div><!--./row -->
+    
+</div><!--./container -->
+
+<!-- Video -->
+  
+ <?php include 'f_footer.php'; ?>
