053fcd58441b09cffabde85a4b54eff81e62a796
S N W Fernando <73053183+NipuniFernando@users.noreply.github.com>
2023-09-05 20:34:41 +0530

    Add files via upload

---
+++ f_about.php
@@ -0,0 +1,82 @@
+<?php include 'f_header.php'; ?>
+
+  <main id="main">
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs" data-aos="fade-in">
+      <div class="container">
+        <h2>ABOUT CKC COLLEGE</h2>
+        <p>Get to know about our College</p>
+      </div>
+    </div><!-- End Breadcrumbs -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <p>About Us</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <h3>CKC COLLEGE - Your Home for Education</h3>
+            <p class="font-italic">
+              As the largest, private sector, higher education network in Sri Lanka, CKC College creates opportunities for over 500 students every year. <br><br>
+              Since our inception in 2019, we have built a solid reputation for delivering high quality and affordable academic programmes ranging from certificate to graduate level, across a variety of disciplines including Computing, Programming, Language Training and Personal and Professional Development.
+            </p>
+            <p>
+              We offer,
+            </p>
+            <ul>
+              <li><i class="icofont-check-circled"></i> Diploma Level Cources</li>
+              <li><i class="icofont-check-circled"></i> Hgher Diploma level Cources</li>
+              <li><i class="icofont-check-circled"></i> Self Learning Online Cources</li>
+            </ul>
+            
+            <a href="f_about.php" class="learn-more-btn">Learn More</a>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Counts Section ======= -->
+    <section id="counts" class="counts section-bg">
+      <div class="container">
+
+        <div class="row counters">
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">15</span>
+            <p>Students</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">12</span>
+            <p>Courses</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">20</span>
+            <p>Events</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">6</span>
+            <p>Trainers</p>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Counts Section -->
+
+
+
+  </main><!-- End #main -->
+
+ <?php include 'f_footer.php'; ?>
---
+++ f_annoucements.php
@@ -0,0 +1,88 @@
+<?php include 'f_header.php'; ?>
+<div class="breadcrumbs" data-aos="fade-in">
+  <div class="container">
+    <h2>Site Annoucements</h2>
+    <p>Latest Annoucemets Of CKC COLLEGE</p>
+  </div>
+</div><!-- End Breadcrumbs -->
+
+<!-- ======= Testimonials Section ======= -->
+<section id="testimonials" class="testimonials">
+  <div class="container" data-aos="fade-up">
+
+    <div class="section-title">
+      <h2>Our Annoucements</h2>
+      <p>Latest News In CKC COLLEGE</p>
+    </div>
+
+    <div class="owl-carousel testimonials-carousel" data-aos="zoom-in" data-aos-delay="100">
+
+      <div class="testimonial-wrap">
+        <div class="testimonial-item">
+          <img src="assets/img/testimonials/Annoucements.jpg" class="testimonial-img" alt="">
+          <h3>New Discount on Diploma Cources</h3>
+          <h4>Promotion</h4>
+          <p>
+            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+            Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+          </p>
+        </div>
+      </div>
+
+      <div class="testimonial-wrap">
+        <div class="testimonial-item">
+          <img src="assets/img/testimonials/promotions.jpg" class="testimonial-img" alt="">
+          <h3>Assignment Submission Extended</h3>
+          <h4>Site News</h4>
+          <p>
+            Assignment Submission of Following cources are extented to 02.03.2021 Due to COVID Situation
+          </p>
+        </div>
+      </div>
+
+      <div class="testimonial-wrap">
+        <div class="testimonial-item">
+          <img src="assets/img/testimonials/Annoucements.jpg" class="testimonial-img" alt="">
+          <h3>Jena Karlis</h3>
+          <h4>Store Owner</h4>
+          <p>
+            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+            Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+          </p>
+        </div>
+      </div>
+
+      <div class="testimonial-wrap">
+        <div class="testimonial-item">
+          <img src="assets/img/testimonials/promotions.jpg" class="testimonial-img" alt="">
+          <h3>Matt Brandon</h3>
+          <h4>Freelancer</h4>
+          <p>
+            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+            Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+          </p>
+        </div>
+      </div>
+
+      <div class="testimonial-wrap">
+        <div class="testimonial-item">
+          <img src="assets/img/testimonials/promotions.jpg" class="testimonial-img" alt="">
+          <h3>John Larson</h3>
+          <h4>Entrepreneur</h4>
+          <p>
+            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+            Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+          </p>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</section><!-- End Testimonials Section -->
+
+<?php include 'f_footer.php'; ?>
